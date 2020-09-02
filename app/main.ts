e946671d00b74997e54b9d9118c01de379e185d8
Ignacio <ilopez-amor@ya.com>
2020-09-02 11:53:36 +0200

    Added autocomplet component

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,10 @@
 import Vue from 'nativescript-vue'
-import Map from './views/Map.vue'
+// import Map from './views/Map.vue'
+import MapComponent from './components/MapComponent.vue'
 import VueDevtools from 'nativescript-vue-devtools'
+
+import RadAutoComplete from 'nativescript-ui-autocomplete/vue'
+Vue.use(RadAutoComplete)
 
 Vue.registerElement('Mapbox', () => require('nativescript-mapbox').MapboxView)
 
@@ -15,5 +19,5 @@ Vue.config.silent = (TNS_ENV === 'production')
 
 new Vue({
 
-  render: h => h('frame', [h(Map)])
+  render: h => h('frame', [h(MapComponent)])
 }).$start()

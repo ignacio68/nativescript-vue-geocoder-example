9d231e4be1b3b5472f60616be7cede2132148293
Ignacio <ilopez-amor@ya.com>
2020-09-08 09:50:04 +0200

    Removed autocomplete plugin

--- app/main.ts
+++ app/main.ts
@@ -3,15 +3,11 @@ import Vue from 'nativescript-vue'
 import MapComponent from './components/MapComponent.vue'
 import VueDevtools from 'nativescript-vue-devtools'
 
-import RadAutoComplete from 'nativescript-ui-autocomplete/vue'
-Vue.use(RadAutoComplete)
-
 Vue.registerElement('Mapbox', () => require('nativescript-mapbox').MapboxView)
 
 if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools)
 }
-
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')

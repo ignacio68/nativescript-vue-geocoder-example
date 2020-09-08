6265c5c4417a7070dddb6867948be7c28cb640f0
Ignacio <ilopez-amor@ya.com>
2020-09-08 18:01:49 +0200

    Deleted unusued services and stores

--- app/services/locationsService.ts
+++
@@ -1,8 +0,0 @@
-import { TokenModel } from 'nativescript-ui-autocomplete'
-
-// export const getLocation = (index: number) => {
-//   const location = locations[index]
-//   return new TokenModel(location, null)
-// }
-
-// export const getLocationsCount = () => this.locations.length

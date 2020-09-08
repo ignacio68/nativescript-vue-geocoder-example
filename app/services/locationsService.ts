43477f2577c2f0b8c433f31df5e720a0ed2bff31
Ignacio <ilopez-amor@ya.com>
2020-09-08 09:45:35 +0200

    Added new services

---
+++ app/services/locationsService.ts
@@ -0,0 +1,8 @@
+import { TokenModel } from 'nativescript-ui-autocomplete'
+
+// export const getLocation = (index: number) => {
+//   const location = locations[index]
+//   return new TokenModel(location, null)
+// }
+
+// export const getLocationsCount = () => this.locations.length

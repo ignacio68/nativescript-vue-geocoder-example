711109a52b5afa2bbb538513db19f092841a6f7d
Ignacio <ilopez-amor@ya.com>
2020-09-16 17:22:45 +0200

    Added formating text util

--- app/utils/text.ts
+++ app/utils/text.ts
@@ -1,9 +1,19 @@
+import { Location } from '@/services/types'
+
 const removeEmptyKeys = (object) => {
   Object.keys(object).forEach((key) => (!object[key] && object[key] !== undefined) && delete object[key])
   return object
 }
 
-export const formatedText = (location) => {
+export const formatedText = (location: Location) => {
   const cleanedLocation = removeEmptyKeys(location)
-  return `${cleanedLocation.thoroughfare} ${cleanedLocation.subThoroughfare} ${cleanedLocation.postalCode} ${cleanedLocation.locality}`
+  if (!cleanedLocation.thoroughfare) {
+    if (!cleanedLocation.country || !cleanedLocation.postalCode) {
+      return `${cleanedLocation.name}`
+    } else {
+      return `${cleanedLocation.name} ${cleanedLocation.postalCode} ${cleanedLocation.country}`
+    }
+  } else {
+    return `${cleanedLocation.thoroughfare} ${cleanedLocation.subThoroughfare} ${cleanedLocation.postalCode} ${cleanedLocation.locality}`
+  }
 }

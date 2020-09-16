91ab96de2e1d26fdb6dfecb8cbeaa6964d925e97
Ignacio <ilopez-amor@ya.com>
2020-09-16 13:49:05 +0200

    Removed search location (single) service

--- app/services/geocodingService.ts
+++ app/services/geocodingService.ts
@@ -2,24 +2,15 @@ import * as geocoding from 'nativescript-geocoding'
 
 import { Location } from './types'
 
-export const searchLocation = async(query: string) => {
-  const fetchLocation = await geocoding.getLocationFromName(query)
-  .then(result =>{
-    let location: Array<Location> = []
-    location.push(result)
-    return location
-    }, error => console.log(`Error: ${error.message || error}`)
-  )
-  return fetchLocation
-}
-
 export const searchLocations = async(query: string) => {
-  const fetchLocations = await geocoding.getLocationListFromName(query, 5)
-  .then(locations => {
+  const fetchLocations: void | Location[] = await geocoding.getLocationListFromName(query, 5)
+  .then((locations: Location[])  => {
     console.log(`Found: ${locations.length}`)
-    let locationsList: Array<Location> = []
-    locations.map(result => locationsList.push(result))
-    return locationsList
+    // console.log(`locations: ${JSON.stringify(locations)}`)
+    // let locationsList: Location[] = []
+    // locations.map(result => locationsList.push(result))
+    // return locationsList
+    return locations
     }, error => console.log(`Error: ${error.message || error}`)
   )
   console.log(`fetchLocations: ${JSON.stringify(fetchLocations)}`)

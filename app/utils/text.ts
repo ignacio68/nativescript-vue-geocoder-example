ee0cb1565ea907f20f3eea20b9a1a778662ddb85
Ignacio <ilopez-amor@ya.com>
2020-09-17 13:39:21 +0200

    Refactored the code to functional programming & changed the format of the direction to shw

--- app/utils/text.ts
+++ app/utils/text.ts
@@ -1,19 +1,27 @@
-import { Location } from '@/services/types'
+import { ShortLocation, Location } from '@/services/types'
+import { pipe } from './functional'
 
-const removeEmptyKeys = (object) => {
+const getShortLocation = (location: Location): ShortLocation => {
+  let shortLocation: ShortLocation = new Object()
+
+  shortLocation.thoroughfare = location.thoroughfare
+  shortLocation.subThoroughfare = location.subThoroughfare
+  shortLocation.locality = location.locality
+  shortLocation.postalCode = location.postalCode
+  shortLocation.country = location.country
+
+  return shortLocation
+}
+
+const removeEmptyKeys = (object: object): object => {
   Object.keys(object).forEach((key) => (!object[key] && object[key] !== undefined) && delete object[key])
   return object
 }
-
-export const formatedText = (location: Location) => {
-  const cleanedLocation = removeEmptyKeys(location)
-  if (!cleanedLocation.thoroughfare) {
-    if (!cleanedLocation.country || !cleanedLocation.postalCode) {
-      return `${cleanedLocation.name}`
-    } else {
-      return `${cleanedLocation.name} ${cleanedLocation.postalCode} ${cleanedLocation.country}`
-    }
-  } else {
-    return `${cleanedLocation.thoroughfare} ${cleanedLocation.subThoroughfare} ${cleanedLocation.postalCode} ${cleanedLocation.locality}`
-  }
+const objectToArray = (object: object): string[] => {
+  const newArray: any[] = Object.values(object)
+  return newArray
 }
+
+const reduceToString = (shortLocationArray): string => `${shortLocationArray.join(', ')}`
+
+export const getShortLocationString = pipe(getShortLocation, removeEmptyKeys, objectToArray, reduceToString)
